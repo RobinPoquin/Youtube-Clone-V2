54b899e3969a4335171853565f3e3fe752ca94a9
unknown <robpoquin@gmail.com>
2026-03-04 20:57:52 +0100

    Création de la barre de recherche dans le Header

---
+++ src/utils/constant.js
@@ -0,0 +1,40 @@
+import MusicNoteIcon from '@mui/icons-material/MusicNote';
+import HomeIcon from '@mui/icons-material/Home';
+import CodeIcon from '@mui/icons-material/Code';
+import OndemandVideoIcon from '@mui/icons-material/OndemandVideo';
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import LiveTvIcon from '@mui/icons-material/LiveTv';
+import SchoolIcon from '@mui/icons-material/School';
+import FaceRetouchingNaturalIcon from '@mui/icons-material/FaceRetouchingNatural';
+import CheckroomIcon from '@mui/icons-material/Checkroom';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import TheaterComedyIcon from '@mui/icons-material/TheaterComedy';
+import FitnessCenterIcon from '@mui/icons-material/FitnessCenter';
+import DeveloperModeIcon from '@mui/icons-material/DeveloperMode';
+
+export const logo = 'https://i.ibb.co/s9Qys2j/logo.png';
+
+export const categories = [
+  { name: 'News', icon: <HomeIcon />, },
+  { name: 'Programmation', icon: <CodeIcon />, },
+  { name: 'Musique', icon: <MusicNoteIcon /> },
+  { name: 'Éducation', icon: <SchoolIcon />, },
+  { name: 'Podcast', icon: <GraphicEqIcon />, },
+  { name: 'Film', icon: <OndemandVideoIcon />, },
+  { name: 'Jeux vidéo', icon: <SportsEsportsIcon />, },
+  { name: 'Lives', icon: <LiveTvIcon />, },
+  { name: 'Sport', icon: <FitnessCenterIcon />, },
+  { name: 'Mode', icon: <CheckroomIcon />, },
+  { name: 'Beauté', icon: <FaceRetouchingNaturalIcon />, },
+  { name: 'Comédie', icon: <TheaterComedyIcon />, },
+  { name: 'Salle de sport', icon: <FitnessCenterIcon />, },
+  { name: 'Crypto', icon: <DeveloperModeIcon />, },
+];
+
+export const demoThumbnailUrl = 'https://laverticale.vivre-a-niort.com/processed/_processed_/a/b/csm_La_Verticale-0018_323e13dc16.jpg';
+export const demoChannelUrl = '/channel/UCyVuTk3_EshrFPFgRNlp73A';
+export const demoVideoUrl = '/video/Tq3U9NaBI6c';
+export const demoChannelTitle = 'Robin qui Grimpe';
+export const demoVideoTitle = "Juste des vidéo d'un gars qui grimpe";
+export const demoProfilePicture = 'http://dergipark.org.tr/assets/app/images/buddy_sample.png'
+
